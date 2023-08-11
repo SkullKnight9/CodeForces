283c1971306a8982e400466cd7150941e47ed6d3
Simar <125185886+SkullKnight9@users.noreply.github.com>
2023-08-12 02:50:51 +0530

    Add files via upload

---
+++ Socks.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,m;
+	cin>>n>>m;
+    int a = n-1;
+    int b = m-1;
+	int result=(a)/(b);
+	cout<<n+result;
+
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+long long x = 1000000000;
+
+long long i;
+
+for(i=1;i<=x;i++){
+    cout<<"1"<<endl;
+
+}
+cout<<"DONE"<<endl;
+
+}
+
